1d6486efdaf0d5dff2d181f38d0abc17092ad44b
Francisco Ossianl.com <ossian.neto@gmail.com>
2020-05-29 18:07:35 -0300

    fixed 1.3

--- The Nature of Code 2/1.3 noise vs random/official example/index.js
+++ The Nature of Code 2/1.3 noise vs random/official example/index.js
@@ -1,5 +1,4 @@
 var xoff1 = 0;
-var xoff2 = 10000;
 
 function setup(){
     createCanvas(400, 400);
@@ -9,11 +8,9 @@ function draw(){
     background(51);
 
     var x = map(noise(xoff1), 0, 1, 0, width);
-    var y = map(noise(xoff2), 0, 1, 0, width);
 
     xoff1 += 0.01;
-    xoff2 += 0.02;
 
-    ellipse(x, y, 24, 24);
+    ellipse(x, 200, 24, 24);
 
 }
